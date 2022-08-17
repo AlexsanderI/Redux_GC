152850ca892c443474f620e656e07041ac25808e
Your Name <y.y.oleksander@gmail.com>
2022-08-17 18:45:08 +0300

    Pagination - React + Redux

--- lesson4/task3/src/Pagination.jsx
+++ lesson4/task3/src/Pagination.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Pagination = ({ currentPage, goPrev, goNext, totalItems, itemsPerPage }) => {
   const noLess = currentPage > 0;
-  const noMore = totalItems / currentPage > 3;
-  console.log(currentPage, itemsPerPage);
+  const noMore = totalItems / currentPage - 1 > 3;
+  console.log(noMore, totalItems, currentPage, itemsPerPage);
   return (
     <div className="pagination">
       <button className="btn" onClick={goPrev} disabled={!noLess}>
