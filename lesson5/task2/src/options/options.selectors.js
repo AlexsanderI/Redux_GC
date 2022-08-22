744f079e47e2b0f37cae81e0c092cf69327f687a
Your Name <y.y.oleksander@gmail.com>
2022-08-23 02:26:41 +0300

    Transfer List

--- lesson5/task2/src/options/options.selectors.js
+++ lesson5/task2/src/options/options.selectors.js
@@ -1,26 +1,19 @@
-// import { createSelector } from 'reselect';
+import { createSelector } from 'reselect';
 
 export const optionsListSelector = state => state.options.optionsList;
 
 export const selectedIdsSelector = state => state.options.selected;
 
-export const selectedOptionsSelector = state => {
-  const allOptionsList = optionsListSelector(state);
-  const selectedIds = selectedIdsSelector(state);
+export const selectedOptionsSelector = createSelector(
+  [optionsListSelector, selectedIdsSelector],
+  (allOptionsList, selectedIds) => {
+    return allOptionsList.filter(option => selectedIds.includes(option.id));
+  },
+);
 
-  return allOptionsList.filter(option => selectedIds.includes(option.id));
-};
-
-// export const selectedOptionsSelector = state => {
-//   const allOptionsList = optionsListSelector(state);
-//   const selectedIds = selectedIdsSelector(state);
-
-//   return allOptionsList.filter(option => selectedIds.includes(option.id));
-// };
-
-export const availableOptionsSelector = state => {
-  const allOptionsList = optionsListSelector(state);
-  const selectedIds = selectedIdsSelector(state);
-
-  return allOptionsList.filter(option => !selectedIds.includes(option.id));
-};
+export const availableOptionsSelector = createSelector(
+  [optionsListSelector, selectedIdsSelector],
+  (allOptionsList, selectedIds) => {
+    return allOptionsList.filter(option => !selectedIds.includes(option.id));
+  },
+);
