eedb9e32772b32e28a0407a1a3211e910d54280d
Your Name <y.y.oleksander@gmail.com>
2022-09-01 15:23:24 +0300

    2

--- lesson6/task3/src/weather/Weather.jsx
+++ lesson6/task3/src/weather/Weather.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import Сities from '../Сities';
+// import Сities from '../Сities';
 import * as citiesActions from './weather.actions';
 import { isCitiesSelector } from './weather.selectors';
 
@@ -16,7 +16,11 @@ const Weather = ({ cityData, getWeatherData }) => {
       <h1 className="weather__title">Weather data</h1>
       <ul className="cities-list">
         {cityData.map(city => (
-          <Сities key={city.id} {...city} />
+          <li className="city" key={city.id}>
+            <span className="city__name">{city.name}</span>
+            <span className="city__temperature">{city.temperature} F</span>
+          </li>
+          // <Сities key={city.id} {...city} />
         ))}
       </ul>
     </main>
